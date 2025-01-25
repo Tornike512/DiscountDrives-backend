bc782999e5888b90b872e665b5373bc6f65b779e
Tornike Tsagareishvili <dht5152@gmail.com>
2025-01-25 15:51:31 +0400

    Add: Basic Dependencies

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
