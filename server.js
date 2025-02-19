a3cb5adab0bb74c0bd4d4925039ee89ed083a582
Tornike Tsagareishvili <dht5152@gmail.com>
2025-02-19 21:12:09 +0400

    Add: Pages Length Count

--- server.js
+++ server.js
@@ -13,8 +13,14 @@ app.use(express.json());
 app.use(cors());
 
 app.get("/", async (req, res) => {
+  const { firstCar, lastCar } = req.query;
+
   try {
-    const cars = await carModel.find().sort({ _id: -1 });
+    const cars = await carModel
+      .find()
+      .sort({ _id: -1 })
+      .skip(firstCar)
+      .limit(lastCar);
 
     if (!cars || cars.length === 0) {
       return res.status(404).json({ message: "No cars were found" });
@@ -23,6 +29,15 @@ app.get("/", async (req, res) => {
   } catch (error) {
     console.error("Error fetching cars:", error);
     res.status(500).json({ message: "Server error" });
+  }
+});
+
+app.get("/page-length", async (req, res) => {
+  try {
+    const pageLength = await carModel.countDocuments();
+    res.status(200).json(pageLength);
+  } catch (error) {
+    res.status(404).json({ message: "Error Fetching Car's Count" });
   }
 });
 
