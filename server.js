668f53467186aba496dc3a78b5c7277599a33290
Tornike Tsagareishvili <dht5152@gmail.com>
2025-03-07 21:56:00 +0400

    Add: Car filtering request

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ import router from "./Routes/CarsRoute.js";
 import mongoose from "mongoose";
 import cors from "cors";
 import carPricesModel from "./Models/carPricesModel.js";
+import carModel from "./Models/carModel.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,51 @@ app.use(cors());
 
 app.use(router);
 
+app.get("/filter-cars", async (req, res) => {
+  try {
+    const { manufacturer, model, year, price } = req.query;
+
+    const cars = await carModel.find({});
+
+    const filterCars = cars.filter((car) => {
+      const manufacturerTerm = manufacturer ? manufacturer.toLowerCase() : "";
+      const modelTerm = model ? model.toLowerCase() : "";
+      const yearTerm = year ? year.toLowerCase() : "";
+      const priceTerm = price ? price.toLowerCase() : "";
+
+      const carModelLower = car.carModel ? car.carModel.toLowerCase() : "";
+      const carYearLower = car.carYear ? String(car.carYear).toLowerCase() : "";
+      const carPriceLower = car.carPrice
+        ? String(car.carPrice).toLowerCase()
+        : "";
+
+      const modelMatch =
+        manufacturer && model
+          ? carModelLower.includes(`${manufacturerTerm} ${modelTerm}`)
+          : false;
+
+      const manuOnlyMatch =
+        manufacturer && !model
+          ? carModelLower.includes(manufacturerTerm)
+          : false;
+
+      const modelOnlyMatch =
+        !manufacturer && model ? carModelLower.includes(modelTerm) : false;
+
+      const yearMatch = year ? carYearLower.includes(yearTerm) : false;
+      const priceMatch = price ? carPriceLower.includes(priceTerm) : false;
+
+      return (
+        modelMatch || manuOnlyMatch || modelOnlyMatch || yearMatch || priceMatch
+      );
+    });
+
+    res.json({ cars: filterCars });
+  } catch (error) {
+    console.error("Error filtering cars:", error);
+    res.status(500).json({ error: "Failed to filter cars" });
+  }
+});
 // app.get("/all-cars", async (req, res) => {
 //   try {
 //     const cars = await carPricesModel.find({});
